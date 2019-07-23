55d5507cbc2973f133130362b3b48d0b7ee0d672
felipe damasceno <felipedamasceno97@gmail.com>
2019-07-23 19:48:28 -0300

    modularizando tudo

--- hero_model.py
+++ hero_model.py
@@ -8,6 +8,7 @@ class Model:
     
     def __init__(self, data, label, most_correlated=0):
         self.X_train, self.X_test, self.y_train, self.y_test = self.__split_data(data, label)
+        #print(set(self.y_train)-set(self.y_test))
         if most_correlated > 0:
             train = pd.concat((self.X_train, self.y_train), axis=1)
             if most_correlated > self.X_train.shape[1]:
@@ -29,7 +30,7 @@ class Model:
         y = data[label]
         X = data.drop([label], axis=1)
         # 75% training and 25% test
-        return train_test_split(X, y, test_size=0.25, random_state=1)
+        return train_test_split(X, y, test_size=0.25, random_state=1, stratify=y)
 
     def __most_correlated(self, data, num, label):
         corr = data.corr()
@@ -50,22 +51,23 @@ class Model:
     def set_best_parameters(self, parameters):
         self.best_parameters = parameters
     
-    def train_test(self):
+    def train_test(self, average='binary'):
         clf = DecisionTreeClassifier(**self.best_parameters, random_state=1)
         clf = clf.fit(self.X_train, self.y_train)    
         y_train_pred = clf.predict(self.X_train)
         y_test_pred = clf.predict(self.X_test)
-        self.report(y_train_pred, y_test_pred)
+        self.report(y_train_pred, y_test_pred, average=average)
         
-    def report(self, y_train_pred, y_test_pred):
+    def report(self, y_train_pred, y_test_pred, average='binary'):
         print("Train:")
-        print("F1 Score:", f1_score(self.y_train, y_train_pred))
+        print("F1 Score:", f1_score(self.y_train, y_train_pred, average=average))
         print()
         print("Test:")
-        print("F1 Score:", f1_score(self.y_test, y_test_pred))
-        tn, fp, fn, tp = confusion_matrix(self.y_test, y_test_pred).ravel()
-        print("True negatives: ", tn)
-        print("True positives: ", tp)
-        print("False negatives: ", fn)
-        print("False positives: ", fp)
+        print("F1 Score:", f1_score(self.y_test, y_test_pred, average=average))
+        if average == 'binary':
+            tn, fp, fn, tp = confusion_matrix(self.y_test, y_test_pred).ravel()
+            print("True negatives: ", tn)
+            print("True positives: ", tp)
+            print("False negatives: ", fn)
+            print("False positives: ", fp)
         
